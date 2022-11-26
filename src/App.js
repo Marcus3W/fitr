41639537891ed76d2345ed6e5492c17cfba1e481
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2022-11-26 12:08:11 +0000

    Added Login.jsx page - Material UI template for 'Sign in side' used for the page - Added background collection to rotate the image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route, BrowserRouter,
+} from "react-router-dom";
+import Login from './pages/Login';
 
 function App() {
-  return (
-    <h1>Fitr</h1>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Login />} />
+                {/*<Route path="/sign-up" element={<Sign-up />} />*/}
+
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
+
